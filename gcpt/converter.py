cebc73a1c60fab1a577ecce490367593c565cf14
sowelilili <atune313@gmail.com>
2024-06-16 21:47:59 +0100

    important fix to convertere

--- gcpt/converter.py
+++ gcpt/converter.py
@@ -1,4 +1,4 @@
-mastercode = "9011CD48 0C0472FA"
+mastercode = "0011CD48 0C0472FA"
 code_lines = [mastercode] + [line.upper().strip() for line in open("bin/38996035.raw") if line != "\n"]
 
 def swap(code):
@@ -8,15 +8,18 @@ def swap(code):
 hexes = []
 for code in code_lines:
     halves = code.split(" ")
-    hexes.append(halves[1])
     hexes.append(halves[0])
+    hexes.append(halves[1])
 
-swapped = [swap(code) for code in hexes]
+swapped = [code for code in hexes]
 joined = "".join(swapped)
 
 result = joined
 
-print("u32 rac2hack[1846] = {")
+output = ""
 for i in range(0, len(result), 8):
-    print(f"\t0x{result[i:i+8]},")
-print("};")
+    output += f"\t0x{result[i:i+8]},\n"
+
+num_lines = len(output.split("\n"))
+output = f"u32 RacTimer[{num_lines - 1}] =" + " {\n" + output + "};"   
+print(output)
